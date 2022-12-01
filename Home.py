22fccc5312ede1de5780cd5b46fce13037b11a55
Jan Markvart <honza.markvart@gmail.com>
2022-12-01 15:03:48 +0100

    Queue page and statistics page

---
+++ Home.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+import numpy as np
+import os
+import glob
+
+###################    EXCEL IMPORT PART     #################
+folder_path = 'C:\\Users\\jan_markvart\\Documents\\Python\\Projects\\Test1st\\data\\OutputData.xlsx'
+#for filename in glob.glob(os.path.join(folder_path, '*.xlsm')):
+dataServiceID = pd.read_excel(folder_path, 'receiptStock')
+del dataServiceID["tel"]
+del dataServiceID["adress"]
+del dataServiceID["ico"]
+del dataServiceID["mail"]
+del dataServiceID["description"]
+#rename columns
+dataServiceID.rename(columns={'protocol_id': 'Číslo protokolu', "customer_id": "Č. zákazníka", "device": "Přístroj", "s_n": "S/N", "name": "Jméno", "status": "Status"},inplace=True)
+
+st.set_page_config(page_title="WB - Servis",
+                    page_icon=":bar_chart:",
+                    layout="wide"
+)
+
+@st.cache
+def get_data_from_excel():
+    df = pd.read_excel(
+        io='dataHistory.xlsx',
+        engine='openpyxl',
+        sheet_name='data',
+        usecols='A:C',
+        nrows=3476,
+    )
+    return df
+df = get_data_from_excel()
+
+# ---- MAINPAGE -----
+st.title("Wöhler Bohemia - Přístroje seznam")
+st.markdown("##")
+
+# TOK KPI's
+st.markdown("---")
+
+# SERVIS BY DEVICE [BAR CHART]
+st.dataframe(dataServiceID)  # Same as st.write(df)
