cb96b22e84a389705d582733c15467836db79d62
Jan Markvart <honza.markvart@gmail.com>
2023-11-01 15:50:19 +0100

    Feat: Change in Home.py source of data to MongoDB

--- Home.py
+++ Home.py
@@ -1,38 +1,37 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-
-
-###################    EXCEL IMPORT PART     #################
-
-#for filename in glob.glob(os.path.join(folder_path, '*.xlsm')):
-
-folder_path = 'OutputData.xlsx'
-dataServiceID = pd.read_excel('OutputData.xlsx', 'receiptStock', dtype={'abno':'int'})
-del dataServiceID["tel"]
-del dataServiceID["adress"]
-del dataServiceID["ico"]
-del dataServiceID["mail"]
-del dataServiceID["description"]
-#rename columns
-dataServiceID.rename(columns={'protocol_id': 'Číslo protokolu', "customer_id": "Č. zákazníka", "device": "Přístroj", "s_n": "S/N", "name": "Jméno", "status": "Status", "abno": "AB číslo"},inplace=True)
+import pymongo
 
 st.set_page_config(page_title="WB - Servis - Příjem přístrojů",
                     page_icon=":bar_chart:",
                     layout="wide"
 )
 
-#@st.cache
-def get_data_from_excel():
-    df = pd.read_excel(
-        io='dataHistory.xlsx',
-        engine='openpyxl',
-        sheet_name='data',
-        usecols='A:C',
-        nrows=3476,
-    )
+# Initialize Mongodb connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return pymongo.MongoClient("mongodb+srv://honzamarkvart:admin@cluster0.ibdbn6l.mongodb.net/")
+
+client = init_connection()
+
+@st.cache_data(ttl=600)
+def get_data_DB():
+    db = client["streamlit-db"]
+    collection = db["protocols"]
+    query11 = {}  # Define your query here if needed
+    data = list(collection.find(query11))
+    df = pd.DataFrame(data)
+    df = df.drop(['_id'], axis=1)
     return df
-df = get_data_from_excel()
+
+df = get_data_DB()
+df = df.drop(['adress', 'ico','mail'], axis=1)
+df['date'] = df['date'].dt.date
+#rename columns
+df.rename(columns={'protocol_id': 'Číslo protokolu', "customer_id": "Č. zákazníka", "device": "Přístroj", "s_n": "S/N", "name": "Jméno", "status": "Status", "abno": "AB číslo", "tel":"Telefon", "date":"Příjem", "description":"Popis"},inplace=True)
+
 
 # ---- MAINPAGE -----
 st.title("Wöhler Bohemia - Příjem")
@@ -41,5 +40,5 @@ st.markdown("##")
 # TOK KPI's
 st.markdown("---")
 
-# SERVIS BY DEVICE [BAR CHART]
-st.dataframe(dataServiceID)  # Same as st.write(df)
+# WRITE TABLE
+st.dataframe(df)  # Same as st.write(df)
